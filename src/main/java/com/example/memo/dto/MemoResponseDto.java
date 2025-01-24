ca233d6c6c688cb993d9ae52db41c53356ac8fb0
3uomlkh <3uomlkh@gmail.com>
2025-01-24 13:42:02 +0900

    feat: MemoResponseDto 클래스 생성자 추가

--- src/main/java/com/example/memo/dto/MemoResponseDto.java
+++ src/main/java/com/example/memo/dto/MemoResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.memo.dto;
 
+import com.example.memo.entity.Memo;
 import lombok.Getter;
 
 @Getter
@@ -7,4 +8,10 @@ public class MemoResponseDto {
     private Long id;
     private String title;
     private String content;
+
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.title = memo.getTitle();
+        this.content = memo.getContent();
+    }
 }
